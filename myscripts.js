1f3b3d88efcb72118ca609e2d799741078ad8575
Laimonas <laimis@Laimonass-Mini.lan>
2019-06-07 12:34:23 +0300

    First page

---
+++ myscripts.js
@@ -0,0 +1,29 @@
+function getById(id){
+	return document.getElementById(id);
+}
+
+function printToElement(id, text){
+	var el = getById(id);
+	el.innerHTML = text;
+}
+
+function getValue(id){
+	return getById(id).value;
+}
+
+function isPalindrome(elValue,elPrint){
+	
+ 	var result = "TAIP.";
+	var resultText = " yra palindrome? ";
+	var palind = "";
+	var world = getValue(elValue).toLowerCase();
+
+	for(var i = 0; i < world.length; i++){
+		palind = palind + world.charAt((world.length - 1) - i); 
+	} 
+	if(world == palind){
+		result = "NE.";
+	}
+	printToElement(elPrint, '"' + world + '"' + resultText + result);
+
+}
