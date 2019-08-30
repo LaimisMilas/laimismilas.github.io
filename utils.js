910910fe78f38477c2bbabfb3266d6f29f7e290e
Laimonas Milašius <laimis.milasius@gmail.com>
2019-08-30 15:11:00 +0300

    Update utils.js

--- utils.js
+++ utils.js
@@ -82,87 +82,27 @@ function moveDown(element){
 
 let Menu = {
      elTmp: function() {
-
          let el = document.createElement("a");
          el.style.margin = "2px";
-
          return el;
      },
      tabs:[
-         { href: "LetterInText.html", text: "LetterInText", index: 0 },
-         { href: "Palindrome.html", text: "Palindrome",index: 1},
-         {
-             href: "RandomeColor.html",
-             text: "RandomeColor",
-             index: 2
-         },
-         {
-             href: "RandomeColor_1.html",
-             text: "RandomeColor_1",
-             index: 3
-         },
-         {
-             href: "bounce-up-down.html",
-             text: "bounce-up-down",
-             index: 4
-         },
-         {
-             href: "bounce.html",
-             text: "bounce",
-             index: 5
-         },
-         {
-             href: "calc.html",
-             text: "calc",
-             index: 6
-         },
-         {
-             href: "console.html",
-             text: "console",
-             index: 7
-         },
-         {
-             href: "nasa-picture.html",
-             text: "nasa-picture",
-             index: 8
-         },
-         {
-             href: "space_control.html",
-             text: "space_control",
-             index: 9
-         },
-         {
-             href: "svg_random_color.html",
-             text: "svg_random_color",
-             index: 10
-         },
-         {
-             href: "textAnalize.html",
-             text: "textAnalize",
-             index: 11
-         },
-         {
-             href: "timediff.html",
-             text: "timediff",
-             index: 12
-         },
-         {
-             href: "temp-mail.html",
-             text: "temp-mail",
-             index: 13
-         },
-         {
-             href: "index.html",
-             text: "home",
-             index: 14
-         },{
-	     href: "proporcijos.html",
-	     text: "proportion",
-	     index: 15	 
-	 },{
-	     href: "ajax-reguest.html",
-	     text: "ajax-reguest",
-	     index: 16
-	 }
+         {href: "LetterInText.html",    text: "LetterInText",    index: 0},
+         {href: "Palindrome.html",      text: "Palindrome",      index: 1},
+         {href: "RandomeColor.html",    text: "RandomeColor",    index: 2},
+         {href: "RandomeColor_1.html",  text: "RandomeColor_1",  index: 3},
+         {href: "bounce-up-down.html",  text: "bounce-up-down",  index: 4},
+         {href: "bounce.html",          text: "bounce",          index: 5},
+         {href: "calc.html",            text: "calc",            index: 6},
+         {href: "console.html",         text: "console",         index: 7},
+         {href: "nasa-picture.html",    text: "nasa-picture",    index: 8},
+         {href: "space_control.html",   text: "space_control",   index: 9},
+         {href: "svg_random_color.html",text: "svg_random_color",index: 10},
+         {href: "textAnalize.html",     text: "textAnalize",     index: 11},
+         {href: "timediff.html",        text: "timediff",        index: 12},
+         {href: "temp-mail.html",       text: "temp-mail",       index: 13},
+         {href: "index.html",           text: "home",            index: 14},
+	 {href: "proporcijos.html",     text: "proportion",      index: 15},
+	 {href: "ajax-reguest.html",    text: "ajax-reguest",    index: 16}
      ]
  }
